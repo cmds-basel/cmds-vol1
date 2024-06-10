e52e87de3d8a5970e73c79db35096566584ec8ca
cmds-basel <165068564+cmds-basel@users.noreply.github.com>
2024-06-10 20:04:23 +0200

    Add files via upload

--- js/sketch.js
+++ js/sketch.js
@@ -4,8 +4,8 @@ function setup() {
   // Setup AR layer
   pixelDensity(1);
   ar = createGraphics(895, 1280, P2D);
-  let canvasElement = document.getElementById('canvas');
-  canvasElement.replaceWith(ar.canvas);
+  let canvasContainer = document.getElementById('canvasContainer');
+  canvasContainer.appendChild(ar.canvas); // Append AR canvas to the container
 
   // Initialize video player (hidden by default)
   let vidFrame = createDiv();
@@ -13,7 +13,7 @@ function setup() {
   vidFrame.position(0, 0);
   vidFrame.size(windowWidth, windowHeight);
   vidFrame.style('display', 'none');
-  vidFrame.html('<iframe id="videoFrame" width="100%" height="100%" src="https://www.youtube.com/embed/?enablejsapi=1&rel=0&autoplay=1&loop=1&controls=0" frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen></iframe>');
+  vidFrame.html('<iframe id="videoFrame" width="100%" height="100%" src="https://www.youtube.com/embed/?enablejsapi=1&rel=0&mute=1&autoplay=1&loop=1&controls=0" frameborder="0" allow="autoplay; encrypted-media;" allowfullscreen></iframe>');
   vidFrame.style('pointer-events', 'none');
   vidFrame.style('z-index', '1'); // Ensure it's above the AR content
 }
